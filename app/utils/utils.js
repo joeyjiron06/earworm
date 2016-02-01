f207e6fa5a658bd2863b26d9f4d4e93a4f65ea30
Joey Jiron <joeyjiron06@gmail.com>
2016-01-31 23:31:00 -0800

    added font-awesome and utils helper

---
+++ app/utils/utils.js
@@ -0,0 +1,51 @@
+export default class Utils {
+  static isNoE(item) {
+
+    if (!Utils.isA(item)) {
+      return true;
+    }
+
+    if (Utils.isArray(item)) {
+      return item.length === 0;
+    }
+
+    if (Utils.isString(item)) {
+      return item.trim() === '';
+    }
+
+    if (Utils.isObject(item)) {
+      return Object.keys(item).length === 0;
+    }
+
+
+    return false;
+  }
+
+  static isA(item) {
+    if (Utils.isNumber(item)) {
+      return !isNaN(item);
+    }
+
+    return item !== null && item !== undefined;
+  }
+
+  static isArray(item) {
+    return Object.prototype.toString.call(item) === '[object Array]';
+  }
+
+  static isString(item) {
+    return Object.prototype.toString.call(item) === '[object String]';
+  }
+
+  static isObject(item) {
+    return Object.prototype.toString.call(item) === '[object Object]';
+  }
+
+  static isNumber(item) {
+    return Object.prototype.toString.call(item) === '[object Number]';
+  }
+
+  static isFunction(item) {
+    return Object.prototype.toString.call(item) === '[object Function]';
+  }
+}
