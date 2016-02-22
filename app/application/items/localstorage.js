cde01d88d7e91ea419aee9d912c53e91742273f3
Joey Jiron <joey.jiron@am.sony.com>
2016-02-21 23:28:59 -0800

    fixed local storage object save issue

--- app/application/items/localstorage.js
+++ app/application/items/localstorage.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import Utils from 'earworm/utils/utils';
 
 const KEYS = {
   'USER' : 'USER'
@@ -18,6 +19,10 @@ export default Ember.Object.extend({
 });
 function save(key, value) {
   try {
+    if (Utils.isObject(value)) {
+      value = '[LS-AUTO-STRANGD]' + JSON.stringify(value);
+    }
+
     window.localStorage.setItem(key, value);
     return true;
   } catch(e) {
@@ -26,7 +31,14 @@ function save(key, value) {
 }
 function retrieve(key) {
   try {
-    return window.localStorage.getItem(key);
+    let val                 = window.localStorage.getItem(key);
+
+    if (Utils.isString(val) && val.indexOf('[LS-AUTO-STRANGD]') === 0) {
+      let lengthOfPrefix    = '[LS-AUTO-STRANGD]'.length;
+      val                   = JSON.parse(val.substring(lengthOfPrefix));
+    }
+
+    return val;
   } catch(e) {
     return undefined;
   }
